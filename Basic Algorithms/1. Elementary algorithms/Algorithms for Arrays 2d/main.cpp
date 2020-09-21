d4be12e186d1c737c6fa897f5823a8cf80a37184
Aandrew94 <aandrei.zamfir@yahoo.com>
2020-09-21 13:51:43 +0300

    main.cpp

--- Basic Algorithms/1. Elementary algorithms/Algorithms for Arrays 2d/main.cpp
+++ Basic Algorithms/1. Elementary algorithms/Algorithms for Arrays 2d/main.cpp
@@ -18,28 +18,24 @@ int main(){
     };
     int i,j;
 
-    /*for (i=0;i < 3; i++){
-        for (j=0; j< 3; j++){
-            std::cin >>array[i][j];
-        }
-    }*/
 
     std::cout<< "\n\n";
 
 //  Show the matrix
-    for (i=0;i < 3; i++){
-        for (j=0; j< 3; j++){
+    for (int i=0;i < 3; i++){
+        for (int j=0; j< 3; j++){
             std::cout<< array[i][j] << "  ";
         }
         std::cout << '\n';
     }
+    std::cout << '\n';
 
-//  Sum and Pod of principal diagonal (a[0][0] - a[1][1] - a[2][2])
+//  Sum and Prod of principal diagonal (a[0][0] - a[1][1] - a[2][2])
     int sum_dig_p{};
     int prod_dif_p{1};
 
-    for (i=0;i < 3; i++){
-        for (j=0; j< 3; j++){
+    for (int i=0;i < 3; i++){
+        for (int j=0; j< 3; j++){
         }
         sum_dig_p += array[i][i];
         prod_dif_p *= array[i][i];
@@ -48,53 +44,98 @@ int main(){
     std::cout<< "prod:\t" << prod_dif_p <<'\n';
     std::cout<< "-----\n";
 
-//  Sum and Pod of secondary diagonal (a[0][2] - a[1][1] - a[2][0])
+//  Sum and Prod of secondary diagonal (a[0][2] - a[1][1] - a[2][0])
     int sum_dig_s{};
     int prod_dif_s{1};
 
-    for (i=0;i < 3; i++){
-        for (j=0; j< 3; j++){
+    for (int i=0;i < 3; i++){
+        for (int j=0; j< 3; j++){
         }
         sum_dig_s += array[i][3-i-1];
         prod_dif_s *= array[i][3-i-1];
     }
     std::cout<< "sum:\t" << sum_dig_s <<'\n';
     std::cout<< "prod:\t" << prod_dif_s <<'\n';
+    std::cout << "------\n";
+
+
+
+std::cout<<"Elementele aflate sub diagonala principala:\n";
+    for(int i=0;i<3;i++){
+        for(int j=0;i>j ;j++){
+            std::cout<<array[i][j]<<" ";
+
+        }
+    std::cout<<'\n';
+    }
+    std::cout<<'\n';
+
+std::cout<<"Elementele aflate deasupra diagonala principala:\n";
+    for(int i=0;i<3;i++){
+        for(int j=i+1; j<3;j++){
+            std::cout<<array[i][j]<<" ";
+
+        }
+    std::cout<<'\n';
+    }
+    std::cout<<'\n';
+
+std::cout<<"Elementele aflate sub diagonala secundara:\n";
+    for(int i=1;i<3;i++){
+        for(int j=3-i; j<3;j++){
+            std::cout<<array[i][j]<<" ";
+
+        }
+    std::cout<<'\n';
+    }
+    std::cout<<'\n';
+
+std::cout<<"Elementele aflate deasupra diagonala secundara:\n";
+    for(int i=1;i<=3;i++){
+        for(int j=1; j<=3-i;j++){
+            std::cout<<array[i-1][j-1]<<" ";
+
+        }
+    std::cout<<'\n';
+    }
+    std::cout<<'\n';
+
+
 
 //  transpusa -> replace lines with colons 
-
-    for (i=0;i < 3; i++){
-        for (j=0; j< 3; j++){
+std::cout<<"Transpus:\n";
+    for (int i=0;i < 3; i++){
+        for (int j=0; j< 3; j++){
             std::cout<< array[j][i] << "  ";
         }
         std::cout << '\n';
     }
 
 //  delete a line 
-    std::cout<<"\n delete line:\t";
+std::cout<<"\n delete line:\t";
     int x; std::cin>> x;
-    for(i = x ; i<3 ;i++){
-        for(j=0;j<3;j++){
+    for(int i = x ; i<3 ;i++){
+        for(int j=0;j<3;j++){
             array[i][j] = array[i+1][j];
         }
     }   //  print the matrix
-    for (i=0;i <3-x; i++){
-        for (j=0; j< 3; j++){
+    for (int i=0;i <3-x; i++){
+        for (int j=0; j< 3; j++){
             std::cout<< array[i][j] << "  ";
         }
         std::cout << '\n';
     }
 
 //  delete a colon    
-    std::cout<<"\n delete colon:\t";
+std::cout<<"\n delete colon:\t";
     int y; std::cin>> y;
-    for(i = 0 ; i<3-x ;i++){
-        for(j=y;j<3;j++){
+    for(int i = 0 ; i<3-x ;i++){
+        for(int j=y;j<3;j++){
             array[i][j] = array[i][j+1];
         }
     }   //  print the matrix
-    for (i=0;i <3-x; i++){
-        for (j=0; j< 3-y; j++){
+    for (int i=0;i <3-x; i++){
+        for (int j=0; j< 3-y; j++){
             std::cout<< array[i][j] << "  ";
         }
         std::cout << '\n';
@@ -112,6 +153,10 @@ int main(){
         prod_dif_s *= array[i][3-j+1];      // coloana mijloc
     }
 */
+
+
+
+
 
 
 void check(int &nr){
